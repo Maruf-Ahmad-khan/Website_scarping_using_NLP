6a85513da5d529806922638997185626db7e457c
Maruf khan <140550280+Maruf-Ahmad-khan@users.noreply.github.com>
2024-12-21 09:50:55 +0000

    First

---
+++ packages/visual.py
@@ -0,0 +1,12 @@
+class Visualize:
+    
+    def __init__(self, df):
+        self.df = df
+        
+        
+    def Freq_count(self):
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+        sns.set_theme
+        import pandas as pd
+        
